a6e909b25055613e7fab2b60184db1962191068b
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-04 14:13:20 -0500

    Mostrar y Ocultar Mensajes de Validación

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,6 +7,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  correcto : boolean = true;
   public formContact!: FormGroup;
   constructor(private formBuilder:FormBuilder) { }
 
