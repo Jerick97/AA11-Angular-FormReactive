0069deebc7b10bdc77066928b07fa18bfc7125a3
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-04 14:22:46 -0500

    Nuevos Cambios

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,7 +7,6 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  correcto : boolean = true;
   public formContact!: FormGroup;
   constructor(private formBuilder:FormBuilder) { }
 
