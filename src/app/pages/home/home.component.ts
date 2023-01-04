45df2548f9491138fcb3073ad02cfee42721c39b
Emerson Suarez <emersonsuarez2904@gmail.com>
2023-01-04 14:00:10 -0500

    Se añadio validaciones en el formulario reactivo

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  public formContact!: FormGroup;
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.formContact = this.formBuilder.group({
+      name: ['', [
+        Validators.required,
+        Validators.minLength(12)
+      ]],
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      message: ['', [
+        Validators.required,
+        Validators.minLength(4),
+        Validators.maxLength(300)
+      ]],
+      
+    });
   }
 
+  send(): any{
+    console.log(this.formContact.value)
+  }
 }
